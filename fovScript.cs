dc7fd1156921cba5557636d768518b179bca58d8
dataEcto <35822132+dataEcto@users.noreply.github.com>
2018-02-13 18:12:08 -0500

    FovScript
    This Script creates a cone and looks for any objects within that cone.
    Right now, I had to use an array like the tutorial I searched up was using, despite the fact the player character is the only one thats suppose to be there. This is just gonna be the initial version.

---
+++ fovScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fovScript : MonoBehaviour {
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+
+    //The layers
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    public List<Transform> snake = new List<Transform>(); //Used for FindVisibleTargets. Actually, The tutorial is using multiple targets. I just dont know how to do one small one
+
+    private void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", .2f);
+    }
+
+    //The CoRoutine
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+
+    }
+
+    void FindVisibleTargets() //Allow us to do something if the player is in the FOV (disregarding walls of course)
+    {
+        //Just so the Array doens't get cluttered and avoid duplicates
+        snake.Clear();
+
+        //Adds targets in view radius to an array
+        Collider2D[] targetsInViewRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius, targetMask);
+
+        //For every targetsInViewRadius it checks if they are inside the field of view
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.up, dirToTarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+                //If line draw from object to target is not interrupted by wall, add target to list of visible 
+                //targets
+                if (!Physics2D.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask)) 
+                {
+                    snake.Add(target);
+
+                }
+            }
+        }
+
+    }
+
+    //This method will take in the Angle, then return the direction of the angle
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) 
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees -= transform.eulerAngles.z;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), 0); //We don't need Z since we're working in 2D
+
+    }
+}
+
