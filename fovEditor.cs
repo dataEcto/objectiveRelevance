99db681a2a8e97a0d82a18d1f671d6a4e950c9d7
dataEcto <35822132+dataEcto@users.noreply.github.com>
2018-02-13 18:16:05 -0500

    fovEditor
    An editor that allows me to see the fov cone in the Scene View
    I can mess around with the size of the editor
    As well as see if its spotting the player via a red line that reaches towards the player character

---
+++ fovEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(fovScript))] //Allow us to visual the fov cone. Accesible via the Scene Editor
+public class FieldOfViewEditor : Editor
+{
+
+    void OnSceneGUI()
+    {
+        fovScript fow = (fovScript)target;
+
+        //Draws view reach
+        Handles.color = Color.yellow;
+        Handles.DrawWireArc(fow.transform.position, Vector3.forward, Vector3.up, 360, fow.viewRadius);
+
+        //Draws cone of view
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+        //A test to see if it truly sees the player
+        Handles.color = Color.red;
+        foreach (Transform snake in fow.snake)
+        {
+            Handles.DrawLine(fow.transform.position, snake.position);
+        }
+    }
+}
