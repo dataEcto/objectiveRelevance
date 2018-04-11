3491cd90c39869686820b91f9b73aef866fef559
Genric Jan Elazegui <gj.elazegui@Gmail.com>
2018-04-11 17:08:55 -0400

    Added a Text Debug scene
    Just so I can mess around with the text.

---
+++ solidusSnake/Assets/dialogueManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dialogueManager : MonoBehaviour {
+
+
+    public Text nameText;
+    public Text dialogueText;
+
+    private Queue<string> sentences;
+
+    //Think: John's Sylladex from Homestuck
+
+    // Use this for initialization
+    void Start() {
+
+        sentences = new Queue<string>();
+
+    }
+
+    public void startDialogue(dialogue dialogue)
+    {
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        displayNextSentence();
+    }
+
+    public void displayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            endDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+
+    }
+
+    void endDialogue()
+    {
+        Debug.Log("end");
+    }
+  
+}
---
+++ solidusSnake/Assets/dialogueTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueTrigger : MonoBehaviour {
+
+    public dialogue dialogue;
+
+    public void triggerDialogue()
+    {
+        FindObjectOfType<dialogueManager>().startDialogue(dialogue);
+    }
+}
