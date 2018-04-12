6f8cf0ee5c0ebfe6a078d0f8be432b1120963698
Genric Jan Elazegui <gj.elazegui@Gmail.com>
2018-04-11 20:45:56 -0400

    Level One in Progress
    I just need to import the actual map into Unity, put down the guards, and then I can add the other aesthetic stuff.

---
+++ solidusSnake/Assets/Tiles/Level 1 Tiles.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Level 1 Tiles" tilewidth="32" tileheight="32" tilecount="28" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="sprite_levelOneTiles06.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="sprite_levelOneTiles07.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="sprite_levelOneTiles08.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="sprite_levelOneTiles09.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="sprite_levelOneTiles10.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="sprite_levelOneTiles11.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="sprite_levelOneTiles12.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="sprite_levelOneTiles13.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="sprite_levelOneTiles14.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="sprite_levelOneTiles15.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="sprite_levelOneTiles16.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="sprite_levelOneTiles17.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="sprite_levelOneTiles18.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="sprite_levelOneTiles19.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="sprite_levelOneTiles20.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="sprite_levelOneTiles21.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="sprite_levelOneTiles22.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="sprite_levelOneTiles23.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="sprite_levelOneTiles24.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="sprite_levelOneTiles25.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="sprite_levelOneTiles26.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="sprite_levelOneTiles27.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="sprite_levelOneTiles00.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="sprite_levelOneTiles01.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="sprite_levelOneTiles02.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="sprite_levelOneTiles03.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="sprite_levelOneTiles04.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="sprite_levelOneTiles05.png"/>
+ </tile>
+</tileset>
